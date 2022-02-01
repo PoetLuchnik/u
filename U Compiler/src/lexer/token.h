d9ab7d585fb56a0a9d9a97fd87cbf29a2ba74405
PoetLuchnik <artem.pirunow@gmail.com>
2022-02-01 20:53:08 +0200

    lexer token begining

---
+++ U Compiler/src/lexer/token.h
@@ -0,0 +1,28 @@
+#ifndef U_LEX_TOKEN_H_INCLUDED
+#define U_LEX_TOKEN_H_INCLUDED
+
+#define U_LEX_TOKENS	\
+	t(UNKNOWN)			\
+	t(NULL)				\
+	t(WHITESPACE)		\
+	t(NEWLINE)			\
+	t(IDENTIFIER)		\
+	t(IF)				\
+	t(ELSE)				\
+	t(EI)				\
+	t(WHILE)			\
+	t(DO)				\
+	t(FOR)				\
+	t(VAR)				\
+	t(STRING)			\
+	t(CHAR)				\
+	t(LINECOMMENT)		\
+	t(COMMENT)			\
+	t(PREPROCESSOR)		\
+	t(SEMICOLON)		\
+	t(BRACE_BEG)		\
+	t(BRACE_END)		\
+	t(PAREN_BEG)		\
+	t(PAREN_END)		\
+
+#endif
