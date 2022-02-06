f317af3406a58d4dbf4c746e28e96e7d8bc8a287
PoetLuchnik <artem.pirunow@gmail.com>
2022-02-06 09:34:29 +0200

    lexer begining: typedef struct LexerData

---
+++ U Compiler/src/lexer/lexer.h
@@ -0,0 +1,14 @@
+#ifndef U_LEXER_H_INCLUDED
+#define U_LEXER_H_INCLUDED
+
+#include"../why/yvector.h"
+#include"token.h"
+
+typedef struct {
+	char* dirname;
+	char* filename;
+	char* data;
+	YV    tokens;
+} LexerData;
+
+#endif
